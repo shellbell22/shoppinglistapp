35c2fc867ce85ff74307a45ac34c35457b1d21c8
shellbell22 <morris.shelley@gmail.com>
2016-07-07 21:25:39 -0400

    retrieval2

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+
+/* Add New Items - ask about how to do a blinking thingy
+*/
+
+/* Adds to list - change bullet */
+
+/* complete button */
+
+/*delete button*/
+
+/* extra:
+
+Add Filter
+Search Box
+List Frequent Items
+Web Storage API
+
+*/
+
+
+
+}
